dbac7fe0b90942073c9889c7221c548782daae91
protoappxyz <shubh.aarsay@gmail.com>
2025-01-29 13:37:16 -0500

    Add error boundary to app

---
+++ src/app/ErrorBoundary.tsx
@@ -0,0 +1,37 @@
+import React, { Component, ReactNode } from "react";
+
+interface ErrorBoundaryProps {
+    children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+        console.error("ErrorBoundary caught an error:", error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div className="flex flex-col items-center justify-center h-screen">
+                    <h1 className="text-2xl font-bold text-red-600">Something went wrong.</h1>
+                    <p className="text-gray-600">Please try again later.</p>
+                </div>
+            );
+        }
+
+        return this.props.children;
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
 'use client'
-import React from 'react';
+import React, {useCallback} from 'react';
 import {VideoListView} from "@/components/VideoListView";
 import {VideoEditor} from "@/components/VideoEditor";
-import {VideoPlayerProvider} from "@/contexts/videoPlayerContext";
+import {ErrorBoundary} from "@/app/ErrorBoundary";
 
 export default function Home() {
   const [videoId, setVideoId] = React.useState('');
+
+  const handleSetVideoId = useCallback((videoId: string) => {
+      setVideoId(videoId);
+  }, []);
   return (
+      <ErrorBoundary>
       <div className="min-h-screen bg-gray-100">
         <div className="container mx-auto p-4">
           <div className="flex flex-col lg:flex-row gap-6">
             <aside className="lg:w-1/4 order-2 lg:order-1">
-              <VideoListView onItemClicked={(videoId) => {
-                setVideoId(videoId);
-              }}/>
+              <VideoListView onItemClicked={handleSetVideoId}/>
             </aside>
-              <main className="sticky top-0 lg:w-3/4 order-1 lg:order-2">
-
-                    <VideoEditor videoId={videoId}/>
+            <main className="lg:w-3/4 order-1 lg:order-2">
+                <VideoEditor videoId={videoId}/>
               </main>
           </div>
         </div>
       </div>
+      </ErrorBoundary>
 );
 }
